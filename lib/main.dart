24407be7e1ddb901e741d7399eeda3722612380c
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-03-02 22:40:41 +0300

    Added question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() {
   runApp(const Quizzler());
@@ -23,6 +24,9 @@ class Quizzler extends StatelessWidget {
   }
 }
 
+/*
+
+ */
 class QuizPage extends StatefulWidget {
   const QuizPage({super.key});
 
@@ -31,37 +35,46 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Question> questionList = [
+    Question(text: 'Some cats are actually allergic to humans.', answer: true),
+    Question(
+        text: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        text: 'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(text: 'A slug\'s blood is green.', answer: true),
+    Question(
+        text: 'Buzz Aldrin\'s mother\'s maiden name was "Moon".', answer: true),
+    Question(
+        text: 'It is illegal to pee in the Ocean in Portugal.', answer: true),
+    Question(
+        text:
+            'No piece of square dry paper can be folded in half more than 7 times.',
+        answer: false),
+    Question(
+        text:
+            'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        answer: true),
+    Question(
+        text:
+            'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        answer: false),
+    Question(
+        text:
+            'The total surface area of two human lungs is approximately 70 square metres.',
+        answer: true),
+    Question(text: 'Google was originally called "Backrub".', answer: true),
+    Question(
+        text:
+            'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        answer: true),
+    Question(
+        text:
+            'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        answer: true)
+  ];
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'Some cats are actually allergic to humans.',
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-    'Buzz Aldrin\'s mother\'s maiden name was "Moon".',
-    'It is illegal to pee in the Ocean in Portugal.',
-    'No piece of square dry paper can be folded in half more than 7 times.',
-    'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-    'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-    'The total surface area of two human lungs is approximately 70 square metres.',
-    'Google was originally called "Backrub".',
-    'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-    'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.'
-  ];
-  List<bool> answers = [
-    true,
-    false,
-    true,
-    true,
-    true,
-    true,
-    false,
-    true,
-    false,
-    true,
-    true,
-    true,
-    true
-  ];
   int questionNumber = 0;
 
   @override
@@ -75,7 +88,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionList[questionNumber].text,
                 textAlign: TextAlign.center,
                 style: const TextStyle(
                   color: Colors.white,
@@ -92,9 +105,8 @@ class _QuizPageState extends State<QuizPage> {
               color: Colors.green,
               onPressed: () {
                 setState(() {
-                  if (questionNumber < questions.length &&
-                      scoreKeeper.length < questions.length) {
-                    if (answers[questionNumber]) {
+                  if (scoreKeeper.length < questionList.length) {
+                    if (questionList[questionNumber].answer) {
                       scoreKeeper.add(
                         const Icon(
                           Icons.check,
@@ -109,9 +121,9 @@ class _QuizPageState extends State<QuizPage> {
                         ),
                       );
                     }
-                    if (questionNumber < questions.length - 1) {
-                      questionNumber++;
-                    }
+                  }
+                  if (questionNumber < questionList.length - 1) {
+                    questionNumber++;
                   }
                 });
               },
@@ -132,9 +144,8 @@ class _QuizPageState extends State<QuizPage> {
               color: Colors.red,
               onPressed: () {
                 setState(() {
-                  if (questionNumber < questions.length &&
-                      scoreKeeper.length < questions.length) {
-                    if (answers[questionNumber]) {
+                  if (scoreKeeper.length < questionList.length) {
+                    if (questionList[questionNumber].answer) {
                       scoreKeeper.add(
                         const Icon(
                           Icons.close,
@@ -149,9 +160,9 @@ class _QuizPageState extends State<QuizPage> {
                         ),
                       );
                     }
-                    if (questionNumber < questions.length - 1) {
-                      questionNumber++;
-                    }
+                  }
+                  if (questionNumber < questionList.length - 1) {
+                    questionNumber++;
                   }
                 });
               },
